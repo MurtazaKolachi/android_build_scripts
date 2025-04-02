47f3b770bcdeaa9be800a5a291daf21870cbb2c2
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-04-03 01:45:19 +0500

    Project Matrixx - Android 15

---
+++ Project Matrixx.sh
@@ -0,0 +1,13 @@
+rm -rf .repo/local_manifests ; \
+rm -rf {device,vendor,kernel,hardware}/xiaomi ; \
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b matrixx device/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
+export BUILD_USERNAME=Murtaza ; \
+export BUILD_HOSTNAME=crave ; \
+export TZ=Asia/Islamabad ; \
+source build/envsetup.sh && \
+brunch apollo
