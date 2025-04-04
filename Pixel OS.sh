c7e22c44fd05ab4e99b05f9cf29ac1dbdc66f3f7
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-04-05 01:59:14 +0500

    Pixel OS - Android 15

--- Pixel OS.sh
+++ Pixel OS.sh
@@ -4,13 +4,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest
 /opt/crave/resync.sh && \
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b pos device/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
 export BUILD_USERNAME=Murtaza ; \
 export BUILD_HOSTNAME=crave ; \
 export TZ=Asia/islamabad ; \
-repo forall -c 'git lfs pull' ; \
 source build/envsetup.sh && \
-lunch aosp_apollo-ap3a-user && \
+lunch aosp_apollo-ap4a-user && \
 make installclean ; \
 mka bacon
