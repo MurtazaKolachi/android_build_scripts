566a1d770242943f645cf1751b8906784002add3
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-04-05 02:02:06 +0500

    AOSPA - Android 15

--- AOSPA.sh
+++ AOSPA.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b vau
 /opt/crave/resync.sh && \
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b vauxite device/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
+git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
 export BUILD_USERNAME=Murtaza ; \
 export BUILD_HOSTNAME=crave ; \
