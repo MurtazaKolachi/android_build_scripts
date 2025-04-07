d8bc95480b16e7cf071dd724cc7c10bb86a7cb89
MurtazaKolachi <murtazakolachi7@gmail.com>
2025-04-08 02:19:33 +0500

    RisingOS Revived

--- RisingOS Revived.sh
+++ RisingOS Revived.sh
@@ -5,10 +5,20 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/andr
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b rising device/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
+git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen-old hardware/xiaomi && \
 export BUILD_USERNAME=Murtaza ; \
 export BUILD_HOSTNAME=crave ; \
 export TZ=Asia/islamabad ; \
 source build/envsetup.sh && \
 riseup apollo user && \
+#Build GMS
 rise b
+To get the build:
+cd out/target/product/apollo
+curl -T "*.zip" -u :6604f309-17e3-4a0d-bde2-87ae3a58eff0 https://pixeldrain.com/api/file/
+#Build Vanilla
+export WITH_GMS=false
+rise b
+To get the build:
+cd out/target/product/apollo
+curl -T "*.zip" -u :6604f309-17e3-4a0d-bde2-87ae3a58eff0 https://pixeldrain.com/api/file/
